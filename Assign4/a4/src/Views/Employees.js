1f628d00c138557777aceccbccddf536f1672f02
Elliot Maude <maude.elliot@gmail.com>
2018-07-05 11:52:29 -0400

    added Headers

--- Assign4/a4/src/Views/Employees.js
+++ Assign4/a4/src/Views/Employees.js
@@ -1,3 +1,14 @@
+/*********************************************************************************
+* WEB422 – Assignment 04
+* I declare that this assignment is my own work in accordance with Seneca Academic Policy. No part of this
+* assignment has been copied manually or electronically from any other source (including web sites) or
+* distributed to other students.
+*
+* Name: Elliot Maude Student ID: 032830127 Date: 7/05/18
+*
+********************************************************************************/ 
+
+
 import React, { Component } from 'react';
 import Axios from 'axios';
 import moment from 'moment';
